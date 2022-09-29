22819cb0baf203148b55f12af6b83cebd98957b6
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-09-29 22:17:56 +0100

    App scaffolded

---
+++ capstone/crwn-clothing/src/App.js
@@ -0,0 +1,49 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/*<img></img>*/}
+        <div className="catgeory-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*<img></img>*/}
+        <div className="catgeory-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*<img></img>*/}
+        <div className="catgeory-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*<img></img>*/}
+        <div className="catgeory-body-container">
+          <h2>Womens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/*<img></img>*/}
+        <div className="catgeory-body-container">
+          <h2>Mens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+    </div>
+
+    
+  )
+}
+
+export default App;
