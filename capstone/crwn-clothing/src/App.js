e1b974cc3e1d8d1ca2ca28434b6a84ed0adfb32a
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-10-04 19:41:01 +0100

    Navigation componenet routed

--- capstone/crwn-clothing/src/App.js
+++ capstone/crwn-clothing/src/App.js
@@ -1,11 +1,32 @@
 import { Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component.jsx';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation Bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return (
+    <h1> its the shop yo </h1>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home/>} />
+        <Route path='shop' element={<Shop/>} />
+      </Route>
     </Routes>
   )
 };
