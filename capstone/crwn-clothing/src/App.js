cf66e161dbd73a32d1e4141983d0695fe25a3bf0
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-10-06 18:17:48 +0100

    Sign in link working

--- capstone/crwn-clothing/src/App.js
+++ capstone/crwn-clothing/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component.jsx';
 import Home from './routes/home/home.component.jsx';
+import SignIn from './routes/sign-in/sign-in.component.jsx';
 
 const Shop = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home/>} />
         <Route path='shop' element={<Shop/>} />
+        <Route path='sign-in' element={<SignIn/>} />
       </Route>
     </Routes>
   )
