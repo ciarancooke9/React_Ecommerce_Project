576859bf2eab82ffa725e5b2b9665160fc7e3ce9
ciarancooke9 <ciaran.cooke9@mail.dcu.ie>
2022-10-17 19:28:38 +0100

    Sign up Working

---
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,70 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHwKjAhU34zl_TMTFOzBoiY60Cd1NPo_I",
+  authDomain: "crwn-clothing-db-89ad7.firebaseapp.com",
+  projectId: "crwn-clothing-db-89ad7",
+  storageBucket: "crwn-clothing-db-89ad7.appspot.com",
+  messagingSenderId: "926317577275",
+  appId: "1:926317577275:web:26880c1585bed24203c81e",
+  measurementId: "G-ZL3YDKYHGV"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
